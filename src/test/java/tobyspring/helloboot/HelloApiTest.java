5b92d67f2fe680feb3e87e09616e618ba1f2d805
권 도윤 <doyoun8813@i-commtech.com>
2023-02-09 00:28:58 +0900

    테스트 코드를 이용한 테스트

---
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -0,0 +1,28 @@
+package tobyspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+public class HelloApiTest {
+
+	@Test
+	void heloApi(){
+
+		TestRestTemplate rest = new TestRestTemplate();
+
+		final ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class,
+			"Spring");
+
+		// status code 200
+		Assertions.assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+		// header(cotent-type) text/plain
+		Assertions.assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+		// body hello Spring
+		Assertions.assertThat(res.getBody()).isEqualTo("Hello Spring");
+	}
+}
