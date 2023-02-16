8bc6a48847a7b4a0103aa5f6b988e956fa817181
권 도윤 <doyoun8813@i-commtech.com>
2023-02-17 00:16:41 +0900

    @Conditional 예외를 잡는 테스트 코드

--- src/test/java/tobyspring/study/ConditionalTest.java
+++ src/test/java/tobyspring/study/ConditionalTest.java
@@ -1,8 +1,10 @@
 package tobyspring.study;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
@@ -15,23 +17,28 @@ public class ConditionalTest {
 	@Test
 	@DisplayName("true 컨디셔널")
 	void conditional(){
-		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
-		ac.register(Config1.class);
-		ac.refresh();
-
-		MyBean bean = ac.getBean(MyBean.class);
-
+		// 스프링 부트에서 제공하는 테스트 전용 ApplicationContext
+		// 테스트 AssertJ 라이브러리 제공하는 유틸리티 클래스
+		ApplicationContextRunner contextRunner = new ApplicationContextRunner();
+		// 적용할 Configuration 넘기고 실행
+		contextRunner.withUserConfiguration(Config1.class)
+			.run(context -> {
+				// Bean 존재 여부 체크
+				assertThat(context).hasSingleBean(MyBean.class);
+				assertThat(context).hasSingleBean(Config1.class);
+			});
 	}
 
 	@Test
 	@DisplayName("false 컨디셔널")
 	void conditional2(){
-		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
-		ac.register(Config2.class);
-		ac.refresh();
-
-		MyBean bean = ac.getBean(MyBean.class);
-
+		ApplicationContextRunner contextRunner = new ApplicationContextRunner();
+		contextRunner.withUserConfiguration(Config2.class)
+			.run(context -> {
+				// Bean 존재하지 않는지 여부 체크
+				assertThat(context).doesNotHaveBean(MyBean.class);
+				assertThat(context).doesNotHaveBean(Config2.class);
+			});
 	}
 
 	@Configuration
@@ -41,7 +48,6 @@ public class ConditionalTest {
 		MyBean myBean() {
 			return new MyBean();
 		}
-
 	}
 
 	@Configuration
@@ -51,7 +57,6 @@ public class ConditionalTest {
 		MyBean myBean() {
 			return new MyBean();
 		}
-
 	}
 
 	static class MyBean {
