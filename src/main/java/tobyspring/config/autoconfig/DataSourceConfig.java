30194c919e5d11aa1d6de4157a4de38d61d0a7d7
권 도윤 <doyoun8813@i-commtech.com>
2023-03-01 13:53:17 +0900

    JdbcTemplate, TransactionManager 구성

--- src/main/java/tobyspring/config/autoconfig/DataSourceConfig.java
+++ src/main/java/tobyspring/config/autoconfig/DataSourceConfig.java
@@ -5,8 +5,12 @@ import java.sql.Driver;
 import javax.sql.DataSource;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.zaxxer.hikari.HikariDataSource;
 
@@ -16,9 +20,11 @@ import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
 // jdbcOperations가 존재하면 자동 구성 컨피그레이션 로딩 시작
-@ConditionalMyOnClass("org.springframework.jdbc.core.jdbcOperations")
+@ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 // 위에 컨디셔널 조건이 되서 사용될 때만 MyDataSourceProperties가 Bean으로 등록
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
+// @Transactional 애노테이션을 찾아 트랜잭션 범위를 활성화한다.
+@EnableTransactionManagement
 public class DataSourceConfig {
 	@Bean
 	@ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource")
@@ -45,6 +51,22 @@ public class DataSourceConfig {
 		dataSource.setPassword(properties.getPassword());
 
 		return dataSource;
+	}
+
+	@Bean
+	// Bean 메서드가 실행될 때 스프링 컨테이너 구성정보에 이 DataSource 타입의 Bean이 딱 한개만 등록되어 있다면
+	// 그 DataSource를 가져와 인자값으로 사용하겠다
+	@ConditionalOnSingleCandidate(DataSource.class)
+	@ConditionalOnMissingBean
+	JdbcTemplate jdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
+	}
+
+	@Bean
+	@ConditionalOnSingleCandidate(DataSource.class)
+	@ConditionalOnMissingBean
+	JdbcTransactionManager jdbcTransactionManager(DataSource dataSource) {
+		return new JdbcTransactionManager(dataSource);
 	}
 
 }
